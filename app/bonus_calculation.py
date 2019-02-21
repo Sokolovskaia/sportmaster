26d60eba989de98fbea26381a33ed5a41c6e5efc
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-02-21 23:01:07 +0300

    Fixed #5.

--- app/bonus_calculation.py
+++ app/bonus_calculation.py
@@ -5,15 +5,13 @@ def bonuses(purchase_amount, previous_purchase_amount):
     input_error = "произошла ошибка начисления"
 
     if 1 < (previous_purchase_amount + purchase_amount) < 15_001:
-        accrued_bonuses = purchase_amount // 1000 * standard_card_bonuses
+        return purchase_amount // 1000 * standard_card_bonuses
 
     elif 15_001 < (previous_purchase_amount + purchase_amount) <= 150_000:
-        accrued_bonuses = purchase_amount // 1000 * silver_card_bonuses
+        return purchase_amount // 1000 * silver_card_bonuses
 
     elif (previous_purchase_amount + purchase_amount) > 150_000:
-        accrued_bonuses = purchase_amount // 1000 * golden_card_bonuses
+        return purchase_amount // 1000 * golden_card_bonuses
 
-    else:
-        accrued_bonuses = input_error
+    return input_error
 
-    return accrued_bonuses
--- app/test/test_bonus_calculation.py
+++ app/test/test_bonus_calculation.py
@@ -2,24 +2,16 @@ from app.bonus_calculation import bonuses
 
 
 def test_bonuses_for_standard_card():
-    accrued_bonuses = bonuses(2_000, 10_000)
-
-    assert 100 == accrued_bonuses
+    assert 100 == bonuses(2_000, 10_000)
 
 
 def test_bonuses_for_silver_card():
-    accrued_bonuses = bonuses(4_500, 100_000)
-
-    assert 280 == accrued_bonuses
+    assert 280 == bonuses(4_500, 100_000)
 
 
 def test_bonuses_for_golden_card():
-    accrued_bonuses = bonuses(1_950, 200_000)
-
-    assert 100 == accrued_bonuses
+    assert 100 == bonuses(1_950, 200_000)
 
 
 def test_bonuses_for_input_errors():
-    accrued_bonuses = bonuses(-1000, 0)
-
-    assert "произошла ошибка начисления" == accrued_bonuses
+    assert "произошла ошибка начисления" == bonuses(-1000, 0)
